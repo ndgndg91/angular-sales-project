8dbbc00137b7464815e63666c7db1594ed78e88c
ndgndg91 <ndgndg91@gmail.com>
2020-09-09 21:01:21 +0900

    In SalesPersonListComponent, create sample data

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  //create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Dong-Gil', 'Nam', 'ndgndg91@gmail.com', 5000),
+    new SalesPerson('Jonh', 'Doe', 'john@gmail.com', 4000),
+    new SalesPerson('Claire', 'Murphy', 'claire@gmail.com', 90000),
+    new SalesPerson('Mai', 'Truong', 'mai@gmail.com', 6000)
+  ];
 
   constructor() { }
 
